d85e56ff6ef8cf7518f5ca4716a34c9476f347f5
Marius Cobzarenco <marius@reinfer.io>
2020-07-04 15:11:09 +0100

    zi: expose border strokes

--- zi/src/components/border.rs
+++ zi/src/components/border.rs
@@ -1,13 +1,12 @@
 use std::iter;
 
-use crate::{
-    layout, Callback, Canvas, Component, ComponentLink, Layout, Rect, ShouldRender, Size, Style,
-};
+use crate::{layout, Canvas, Component, ComponentLink, Layout, Rect, ShouldRender, Size, Style};
 
 #[derive(Clone)]
 pub struct BorderProperties {
     pub component: Layout,
     pub style: Style,
+    pub stroke: BorderStroke,
     pub title: Option<(String, Style)>,
 }
 
@@ -16,12 +15,18 @@ impl BorderProperties {
         Self {
             component,
             style: Style::default(),
+            stroke: BorderStroke::default(),
             title: None,
         }
     }
 
     pub fn style(mut self, style: impl Into<Style>) -> Self {
         self.style = style.into();
+        self
+    }
+
+    pub fn stroke(mut self, stroke: BorderStroke) -> Self {
+        self.stroke = stroke;
         self
     }
 
@@ -55,58 +60,64 @@ impl Component for Border {
     }
 
     fn view(&self) -> Layout {
-        let border = BorderStroke::light_rounded();
+        let stroke = &self.properties.stroke;
 
+        // Draw the top border
         let mut top_border = Canvas::new(Size::new(self.frame.size.width, 1));
         top_border.draw_graphemes(
             0,
             0,
             self.properties.style,
-            iter::once(border.top_left_corner)
+            iter::once(stroke.top_left_corner)
                 .chain(
-                    iter::repeat(border.top_horizontal)
+                    iter::repeat(stroke.top_horizontal)
                         .take(self.frame.size.width.saturating_sub(2)),
                 )
-                .chain(iter::once(border.top_right_corner)),
+                .chain(iter::once(stroke.top_right_corner)),
         );
 
+        // Draw title if present
         if let Some(title) = self.properties.title.as_ref() {
             top_border.draw_str(2, 0, title.1, &title.0);
         }
 
+        // Draw right border
         let mut right_border = Canvas::new(Size::new(1, self.frame.size.height.saturating_sub(2)));
         (0..self.frame.size.height.saturating_sub(2)).for_each(|y| {
             right_border.draw_graphemes(
                 0,
                 y,
                 self.properties.style,
-                iter::once(border.right_vertical),
+                iter::once(stroke.right_vertical),
             );
         });
 
+        // Draw bottom border
         let mut bottom_border = Canvas::new(Size::new(self.frame.size.width, 1));
         bottom_border.draw_graphemes(
             0,
             0,
             self.properties.style,
-            iter::once(border.bottom_left_corner)
+            iter::once(stroke.bottom_left_corner)
                 .chain(
-                    iter::repeat(border.bottom_horizontal)
+                    iter::repeat(stroke.bottom_horizontal)
                         .take(self.frame.size.width.saturating_sub(2)),
                 )
-                .chain(iter::once(border.bottom_right_corner)),
+                .chain(iter::once(stroke.bottom_right_corner)),
         );
 
+        // Draw left border
         let mut left_border = Canvas::new(Size::new(1, self.frame.size.height.saturating_sub(2)));
         (0..self.frame.size.height.saturating_sub(2)).for_each(|y| {
             left_border.draw_graphemes(
                 0,
                 y,
                 self.properties.style,
-                iter::once(border.left_vertical),
+                iter::once(stroke.left_vertical),
             );
         });
 
+        // Assemble layout
         layout::column([
             layout::fixed(1, top_border.into()),
             layout::auto(layout::row([
@@ -119,7 +130,8 @@ impl Component for Border {
     }
 }
 
-struct BorderStroke {
+#[derive(Clone, Debug)]
+pub struct BorderStroke {
     pub top_left_corner: char,
     pub top_horizontal: char,
     pub top_right_corner: char,
@@ -130,8 +142,14 @@ struct BorderStroke {
     pub right_vertical: char,
 }
 
+impl Default for BorderStroke {
+    fn default() -> Self {
+        Self::light_rounded()
+    }
+}
+
 impl BorderStroke {
-    const fn light_rounded() -> Self {
+    pub const fn light_rounded() -> Self {
         Self {
             top_left_corner: '╭',
             top_horizontal: '─',
@@ -144,7 +162,7 @@ impl BorderStroke {
         }
     }
 
-    const fn block() -> Self {
+    pub const fn block() -> Self {
         Self {
             top_left_corner: '█',
             top_horizontal: '▀',
@@ -157,7 +175,7 @@ impl BorderStroke {
         }
     }
 
-    const fn heavy() -> Self {
+    pub const fn heavy() -> Self {
         Self {
             top_left_corner: '┏',
             top_horizontal: '━',
@@ -170,7 +188,7 @@ impl BorderStroke {
         }
     }
 
-    const fn double() -> Self {
+    pub const fn double() -> Self {
         Self {
             top_left_corner: '╔',
             top_horizontal: '═',
