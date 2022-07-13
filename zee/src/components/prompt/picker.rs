ac9bdc251bc9ecbe9ddb5ecdd569959e732a1e2b
Iain H <iain@spiralpoint.org>
2022-07-12 22:25:15 -0400

    Update enter behaviour to handle file creation
    The previous change broke the behaviour of creating files when the
    user entered a new filename. This change corrects this by creating
    files if the no item is selected in the picker.
    
    The behaviour when the user presses enter is now:
    
      1. If a file is selected, open it.
      2. If a directory is selected, navigate into it and refresh the picker
         view.
      3. If no file or directory is selected (user has entered a filename
         that does not "fuzzy" match an existing directory or filename) the
         file is created.

--- zee/src/components/prompt/picker.rs
+++ zee/src/components/prompt/picker.rs
@@ -144,24 +144,27 @@ impl Component for FilePicker {
     fn update(&mut self, message: Message) -> ShouldRender {
         let initial_height = self.height();
         let input_changed = match message {
-            Message::OpenFile => {
-                if let Some(path) = self.listing.selected(self.selected_index) {
-                    if path.is_dir() {
-                        self.input = path.to_string_lossy().into();
-                        ensure_trailing_newline_with_content(&mut self.input);
-                        self.cursor.move_to_end_of_line(&self.input);
-                        self.cursor.insert_char(&mut self.input, '/');
-                        self.cursor.move_right(&self.input);
-                        self.selected_index = 0;
-                        true
-                    } else {
-                        self.properties.on_open.emit(path.to_path_buf());
-                        false
-                    }
-                } else {
+            Message::OpenFile => match self.listing.selected(self.selected_index) {
+                Some(path) if path.is_dir() => {
+                    self.input = path.to_string_lossy().into();
+                    ensure_trailing_newline_with_content(&mut self.input);
+                    self.cursor.move_to_end_of_line(&self.input);
+                    self.cursor.insert_char(&mut self.input, '/');
+                    self.cursor.move_right(&self.input);
+                    self.selected_index = 0;
+                    true
+                }
+                Some(path) => {
+                    self.properties.on_open.emit(path.to_path_buf());
                     false
                 }
-            }
+                None => {
+                    let path_str: Cow<str> = self.input.slice(..).into();
+                    let path = PathBuf::from(path_str.trim());
+                    self.properties.on_open.emit(path);
+                    false
+                }
+            },
             Message::SelectParentDirectory => {
                 let path_str: String = self.input.slice(..).into();
                 self.input = Path::new(&path_str.trim())
